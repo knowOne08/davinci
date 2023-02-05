3ee9122667f60c5a2d3b277a3e83c27670b230dd
knowOne08 <yashdarji23082004@gmail.com>
2023-02-05 17:18:23 +0530

    changed the max_tokens

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ client.on('messageCreate', async (msg)=>{
       const gptResponse = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: text,
-      max_tokens: 300,
+      max_tokens: 512,
       temperature:0.5, 
       stop: ['ChatGPT:', 'achillies:', 'stopPlease:']
     })
